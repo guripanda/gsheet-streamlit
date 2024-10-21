84b96238c6907e8670d53a6b7b2794f415363f5f
guripanda <109257306+guripanda@users.noreply.github.com>
2024-10-21 16:58:06 +0900

    Update app.py

--- app.py
+++ app.py
@@ -322,8 +322,8 @@ def preprocess_and_visualize(data, selected_grade):
         # Display the DataFrame
         st.markdown(f"**<{selected_grade} 학년 학생 미래역량 평균 점수>**", unsafe_allow_html=True)
         st.markdown(overall_avg_df.to_html(index=False).replace('<th>', '<th style="text-align: center;">'), unsafe_allow_html=True)
-        print(filtered_data.head())
-        print(filtered_data['학년'].unique())
+        st.write(print(filtered_data.head()))
+        st.write(print(filtered_data['학년'].unique()))
 
         # Melt the DataFrame for plotting
         overall_avg_df2=overall_avg_df[['역량', '본교_평균', '대구_평균']].iloc[:-1]
